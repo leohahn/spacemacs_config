6b28dbbb328c25a8d5820ec371226a3c5a869a25
Leonardo Hahn <leo_lh324@hotmail.com>
2016-07-31 20:07:09 +0200

    first commit.

---
+++ config.el
@@ -0,0 +1,24 @@
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+
+This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(defun dotspacemacs/user-config ()
+  "Configuration function for user code.
+This function is called at the very end of Spacemacs initialization after
+layers configuration.
+This is the place where most of your configurations should be done. Unless it is
+explicitly specified that a variable should be set before a package is loaded,
+you should place your code here."
+  (add-hook 'scala-mode-hook (lambda () (rainbow-delimiters-mode -1)) t)
+  (add-hook 'elixir-mode-hook (lambda () (rainbow-delimiters-mode -1)) t)
+  (setq css-indent-offset 2)
+  (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
+  (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
+  (global-linum-mode)
+  (set-exec-path-from-shell-PATH)
+  )
